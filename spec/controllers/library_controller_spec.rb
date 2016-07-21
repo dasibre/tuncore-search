4460f587d7dd662091f02ff9b0aa5f110977bfbc
James Naadjie <james.naadjie@gmail.com>
2016-07-21 18:29:55 -0400

    search specs

---
+++ spec/controllers/library_controller_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe LibraryController, type: :controller do
+
+  describe 'GET #index' do
+    it 'returns http success' do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+
+	  it 'returns list of results for given query' do
+		  get :index, {search: 'Hankuna Matata'}
+		  expect(assigns(:results).size).to eq(1)
+	  end
+  end
+
+end
