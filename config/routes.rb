5fcfa969dc361541fc1c2c688da8cacc0880a564
James Naadjie <james.naadjie@gmail.com>
2016-07-21 17:48:22 -0400

    add root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'library/index'
+
+	root 'library#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
