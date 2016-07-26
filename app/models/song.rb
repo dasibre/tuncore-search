7666702d2419478473769c9b120a6824ebf32cfa
James Naadjie <james.naadjie@gmail.com>
2016-07-26 01:03:31 -0400

    refactor sanitize query method

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,6 +2,17 @@ class Song < ActiveRecord::Base
   belongs_to :album
   belongs_to :artist
 
+  class << self
+    def text_search(query)
+      joins(:artist, :album).where("songs.search_vector @@ #{sanitize_query(query)} or artists.search_vector @@ #{sanitize_query(query)} or albums.search_vector @@ #{sanitize_query(query)}")
+    end
+
+    private
+
+    def sanitize_query(query)
+      sanitize_sql_array(["to_tsquery('english', ?)", query.downcase.gsub(/\s/, "|")])
+    end
+  end
 
   def artist_name
     artist.name
