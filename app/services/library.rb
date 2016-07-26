19eceb778922f0e13189d69e1adfc852be6a802c
James Naadjie <james.naadjie@gmail.com>
2016-07-25 21:46:39 -0400

    sanitize query

--- app/services/library.rb
+++ app/services/library.rb
@@ -1,10 +1,18 @@
 class Library
 	class << self
+		def all
+			Song.all
+		end
+		
 		def search(query)
-			songs = Song.text_search(query)
-			artists = Artist.text_search(query)
-			albums = Album.text_search(query)
-			results = songs.concat(artists.concat(albums)).uniq
+			sanitized = sanitize_sql(["to_tsquery('english', ?)", query.downcase.gsub(/\s/, "|")])
+			Song.joins(:artist, :album).where("songs.search_vector @@ #{sanitized} or artists.search_vector @@ #{sanitized} or albums.search_vector @@ #{sanitized}")
+		end
+
+		private
+
+		def sanitize_sql(arry_str)
+			ActiveRecord::Base.send(:sanitize_sql_array, arry_str)
 		end
 	end
 end
