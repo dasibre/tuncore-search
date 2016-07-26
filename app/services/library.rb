b35e9f1e81921799de87c80e7bd6884c133e0003
James Naadjie <james.naadjie@gmail.com>
2016-07-25 22:14:47 -0400

    refactor library

--- app/services/library.rb
+++ app/services/library.rb
@@ -5,14 +5,7 @@ class Library
 		end
 		
 		def search(query)
-			sanitized = sanitize_sql(["to_tsquery('english', ?)", query.downcase.gsub(/\s/, "|")])
-			Song.joins(:artist, :album).where("songs.search_vector @@ #{sanitized} or artists.search_vector @@ #{sanitized} or albums.search_vector @@ #{sanitized}")
-		end
-
-		private
-
-		def sanitize_sql(arry_str)
-			ActiveRecord::Base.send(:sanitize_sql_array, arry_str)
+			Song.text_search(query)
 		end
 	end
 end
